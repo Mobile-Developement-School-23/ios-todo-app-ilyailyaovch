dc0e645b082a3b98d918f22e1171654af2de797f
Ilya Ovchinnikov <jewovch@gmail.com>
2023-07-01 21:06:12 +0300

    code style

--- TodoListSHMR/Style/Colors.swift
+++ TodoListSHMR/Style/Colors.swift
@@ -3,29 +3,29 @@ import UIKit
 enum Colors: String {
 
     //  Support colors
-    case supportSeparator   = "supportSeparator"
-    case supportOverlay     = "supportOverlay"
-    case supportNavBarBlur  = "supportNavBarBlur"
+    case supportSeparator = "supportSeparator"
+    case supportOverlay = "supportOverlay"
+    case supportNavBarBlur = "supportNavBarBlur"
 
     //  Label colors
-    case labelPrimary   = "labelPrimary"
+    case labelPrimary = "labelPrimary"
     case labelSecondary = "labelSecondary"
-    case labelTertiary  = "labelTertiary"
-    case labelDisable   = "labelDisable"
+    case labelTertiary = "labelTertiary"
+    case labelDisable = "labelDisable"
 
     //  Colors
-    case red        = "red"
-    case green      = "green"
-    case blue       = "blue"
-    case gray       = "gray"
-    case grayLight  = "grayLight"
-    case white      = "white"
+    case red = "red"
+    case green = "green"
+    case blue = "blue"
+    case gray = "gray"
+    case grayLight = "grayLight"
+    case white = "white"
 
     //  Background colors
     case backIOSPrimary = "backIOSPrimary"
-    case backPrimary    = "backPrimary"
-    case backSecondary  = "backSecondary"
-    case backElevated   = "backElevated"
+    case backPrimary = "backPrimary"
+    case backSecondary = "backSecondary"
+    case backElevated = "backElevated"
 
     var color: UIColor { return UIColor(named: rawValue) ?? .orange}
 }
